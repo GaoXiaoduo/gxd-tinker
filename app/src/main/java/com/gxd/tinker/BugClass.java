16b2b52fc9ee2447ed204f485918857ebba341ef
GaoXiaoduo <84790857@qq.com>
2018-12-25 10:16:01 +0800

    add button

---
+++ app/src/main/java/com/gxd/tinker/BugClass.java
@@ -0,0 +1,21 @@
+package com.gxd.tinker;
+
+import android.util.Log;
+
+/**
+ * 测试bug类
+ *
+ * @author gaoxiaoiduo
+ * @version 1.0
+ * @date 18/12/21下午3:04
+ */
+public class BugClass
+{
+    public String bug ()
+    {
+        // 这段代码会报空指针异常
+        String str = null;
+        Log.e("BugClass", "get String length:" + str.length());
+        return "This is a bug class";
+    }
+}
---
+++ app/src/main/java/com/gxd/tinker/LoadBugClass.java
@@ -0,0 +1,23 @@
+package com.gxd.tinker;
+
+/**
+ * 加载Bug的类
+ *
+ * @author gaoxiaoiduo
+ * @version 1.0
+ * @date 18/12/21下午3:08
+ */
+public class LoadBugClass
+{
+    /**
+     * 获取bug字符串.
+     *
+     * @return 返回bug字符串
+     */
+    public static String getBugString ()
+    {
+
+        BugClass bugClass = new BugClass();
+        return bugClass.bug();
+    }
+}
